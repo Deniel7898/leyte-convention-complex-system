ad750d97d8f543e1c8a53260c7d6a864df6dc5bf
Deniel7898 <laurentedenielpaul@gmail.com>
2026-03-17 09:07:34 +0800

    🗑️ Removing QR Code Page Removed the QR code page and related components from the Leyte Convention Complex System. This includes: - Deleted the QR code page view and associated frontend components. - Removed related routes pointing to the QR code page. - Cleaned up unused references connected to the QR code page. - Updated navigation and system structure after the removal.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,20 +82,6 @@
             </a>
             <ul id="ddmenu_1" data-bs-parent="#sidebarAccordion" class="dropdown-nav collapse mt-1 ps-4 @if(collect(['qr_codes.*','categories.*','units.*'])->contains(fn($pattern) => request()->routeIs($pattern))) show @endif">
                 <li>
-                    <a href="{{ route('qr_codes.index') }}" title="{{ __('QR Codes & Labels') }}" class="@if(request()->routeIs('qr_codes.*')) active @endif">
-                        <span class="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-qr-code" viewBox="0 0 16 16">
-                                <path d="M2 2h2v2H2z" />
-                                <path d="M6 0v6H0V0zM5 1H1v4h4zM4 12H2v2h2z" />
-                                <path d="M6 10v6H0v-6zm-5 1v4h4v-4zm11-9h2v2h-2z" />
-                                <path d="M10 0v6h6V0zm5 1v4h-4V1zM8 1V0h1v2H8v2H7V1zm0 5V4h1v2zM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8zm0 0v1H2V8H1v1H0V7h3v1zm10 1h-1V7h1zm-1 0h-1v2h2v-1h-1zm-4 0h2v1h-1v1h-1zm2 3v-1h-1v1h-1v1H9v1h3v-2zm0 0h3v1h-2v1h-1zm-4-1v1h1v-2H7v1z" />
-                                <path d="M7 12h1v3h4v1H7zm9 2v2h-3v-1h2v-1z" />
-                            </svg>
-                        </span>
-                        <span class="text">{{ __('QR Codes & Labels') }}</span>
-                    </a>
-                </li>
-                <li>
                     <a href="{{ route('categories.index') }}" title="{{ __('Categories') }}" class="@if(request()->routeIs('categories.*')) active @endif">
                         <span class="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
